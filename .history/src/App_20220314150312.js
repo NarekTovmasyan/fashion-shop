3444773ba02e8d3e49a9c2ec255a32399a30956e
Narek Tovmasyan <96180347+NarekTovmasyan@users.noreply.github.com>
2022-03-14 16:47:51 +0400

    fashion shop start

---
+++ .history/src/App_20220314150312.js
@@ -0,0 +1,15 @@
+// import logo from "./logo.svg";
+import Header from "./components/header/Header";
+import "./App.css";
+import Footer from "./components/footer/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ .history/src/components/header/Header_20220314142336.jsx
@@ -0,0 +1,19 @@
+
+function Header() {
+  return (
+    <div>
+    <Menu inverted>
+      <Menu.Item name="home" active={true} />
+      <Menu.Item name="messages" active={false} />
+      <Menu.Item name="friends" active={false} />
+      <Menu.Menu position="right">
+        <Menu.Item name="logout" active={false} />
+      </Menu.Menu>
+    </Menu>
+
+    <Segment>
+      {/* <img src="/images/wireframe/media-paragraph.png" /> */}
+    </Segment>
+  </div>
+  )
+}
